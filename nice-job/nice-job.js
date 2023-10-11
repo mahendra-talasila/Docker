586c38c63de8794af231077b2f2fe0fc3bd83cd1
mahendra-talasila <145710592+mahendra-talasila@users.noreply.github.com>
2023-10-11 16:22:20 -0400

    Update nice-job.js

--- nice-job/nice-job.js
+++ nice-job/nice-job.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/newplace', (req, res) => {
+app.get('/nicejob', (req, res) => {
   res.send("This is  nice-job!")
 })
 
